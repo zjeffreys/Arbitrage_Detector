850e2afae303d7597fc62b1ff44023e60f273314
zjeffreys <zjeffreys@seattlu.edu>
2022-10-24 12:28:19 -0700

    Data is parse onto Bellman-Ford

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Lab 3
 Goal: Create a subscriber that subscribes to changes in currency values from the 
 forex_provider. If a change is detected, use the Bellman Ford algorihtm to determine if 
 there is an arbitrage. 
+
+1) Run Subscriber
+    - python3 forex_provider_v2.py
+
+2) Run lab3.py
+    - a) Create UDP socket that can get print serialized data from forex_provider_v2.py
+    
--- lab3.py
+++ lab3.py
@@ -1,13 +1,42 @@
 import socket
 import sys
+import fxp_bytes
+import fxp_bytes_subscriber
+import ipaddress
+from array import array
+
+
+REQUEST_ADDRESS = ('127.0.0.1', 50403)
+DEFAULT_ADDRESS = ('127.0.0.1', 0)
+TEST_ADDRESS = ('127.0.0.1', 65534)
+
 
 class Lab3(object):
-    def __init__(self, gcd_host, gcd_port):
-        print("constructor")
+    def __init__(self):
+        self.subscribe_to_forex()
+
+    def subscribe_to_forex(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.bind(DEFAULT_ADDRESS)
+            myAddr = (DEFAULT_ADDRESS[0], s.getsockname()[1])
+            while True:
+                s.sendto(fxp_bytes_subscriber.serialize_address(myAddr), REQUEST_ADDRESS)
+                data, addr = s.recvfrom(4096)
+                decoded = fxp_bytes_subscriber.de_marshal_message(data)
+            # print(data)
+            # continue from here 
+    
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: python lab1.py GCDHOST GCDPORT")
+    if len(sys.argv) != 1:
+        # print("Usage: python lab3.py GCDHOST GCDPORT")
+        print("Usage: python3 lab3.py")
         exit(1)
-    print("Hello from lab3.py")
+    lab3 = Lab3()
+    
+    
+
+
+
+
